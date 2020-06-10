5d3b2aec2d68c51b0c8be4448e30f9ea8ad4e8c6
AlexandreARXS <alexandrearxs@gmail.com>
2020-06-10 10:10:24 -0300

    Script inimigo n finalizada

--- Assets/Scripts/InimigoHorizontal.cs
+++ Assets/Scripts/InimigoHorizontal.cs
@@ -2,49 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-//public class InimigoHorizontal : MonoBehaviour
-//{
-  //  private bool colidde = false;
+public class InimigoHorizontal : MonoBehaviour
+{
+    private bool colidde = false;
 
-   // private float move = -2;
+    private float move = -2;
     
-    //void Start()
-    //{
-    //}
+    void Start()
+    {
+    }
 
-    //void Update()
-    //{
-      //  Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+    void Update()
+    {
+      Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
       
-      //  SpriteRenderer spriterender = GetComponent<SpriteRenderer>();
+      SpriteRenderer spriterenderer = GetComponent<SpriteRenderer>();
 
-      //  rigidbody.velocity = new Vector2(move, rigidbody.velocity.y);
-      //  if (colidde)
-        //{
-        //    Flip();
-        //}
-    //}
+        rigidbody.velocity = new Vector2(move, rigidbody.velocity.y);
+        if (colidde)
+        {
+            Flip();
+        }
+    }
 
-//    private void Flip()
- //   {
-   //     move *= -1;
-     //   spriterender.flipX = !spriterender.flipX;
-       // colidde = false;
-    //}
+    private void Flip()
+    {
+      move *= -1;
+        spriterenderer.flipX = !spriterenderer.flipX;
+        colidde = false;
+    }
 
-//    void OnCollisionEnter2D(Collision2D col)
-  //  {
-    //    if (col.gameObject.CompareTag("Plataforma"))
-      //  {
-        //    collide = true;
-     //   }
-    //}
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Plataforma"))
+        {
+            colidde = true;
+        }
+    }
 
-    //void OnCollisionExit2D(Collision2D col)
-    //{
-      //  if (col.gameObject.CompareTag("Plataforma"))
-        //{
-          //  colidde = false;
-        //}
-   // }
-//}
+    void OnCollisionExit2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Plataforma"))
+        {
+           colidde = false;
+        }
+   }
+}
